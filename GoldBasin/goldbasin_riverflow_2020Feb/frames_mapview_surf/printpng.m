d28735f76b2db93e13135576725190dd2dac7038
David George <dave.jorge@gmail.com>
2020-03-04 13:19:10 -0800

    matlab scripts for plotting surface elevation

---
+++ GoldBasin/goldbasin_riverflow_2020Feb/frames_mapview_surf/printpng.m
@@ -0,0 +1,13 @@
+function printpng(fname)
+
+% PRINTJPG prints a figure as a .jpg
+%
+%     PRINTJPG(FNAME) makes a small 3in x 3in figure suitable for
+%     putting on a webpage, and that looks better than what is obtained
+%     by shrinking down the standard output from print -djpg
+%
+% See also MAKEFRAMEGIF, PRINTJPG, and the unix command CONVERT.
+
+fig = gcf;
+%fig.PaperPosition = [0 0 16 10];
+eval(['print -dpng ' fname]);
