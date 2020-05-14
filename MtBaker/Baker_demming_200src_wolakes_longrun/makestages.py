9fd89aed9e145816c6d0366b4064107cc9ac24a1
David George <dave.jorge@gmail.com>
2020-05-14 13:18:59 -0700

    makestages debug...complete for now

--- MtBaker/Baker_demming_200src_wolakes_longrun/makestages.py
+++ MtBaker/Baker_demming_200src_wolakes_longrun/makestages.py
@@ -1,82 +1,107 @@
-#import matplotlib.pyplot as plt
 import os
 import numpy as np
-#import clawtools.gaugedata as cg
-#import geotools.topotools as gt
-import clawtools.fortconvert as cf
+import dclaw.fortconvert as cf
+"""
+this routine creates time series output at chosen locations from fort.q files.
+for time series with higher time resolution than fort output use the gauge feature prior to runtime
+script is intended to be run from top-level simulation directory above _output/.
+"""
 
-xdam = (5.526e5,5.18325e6)
-xconf = (5.7385e5,5.1945e6)
-xpn = (5.76345e5,5.1958e6)
-xps = (5.76388e5,5.1924e6)
-xort = (5.602e5,5.215e6)
-xash = (5.733e5,5.1772e6)
+#get current directory
+pwd = os.path.abspath('./')
 
-infiles = ['GaugePuyallupSouth.txt','GaugePuyallupNorth.txt','GaugePuyallupConfluence.txt','GaugeOrting.txt','GaugeAshford.txt','GaugeAlderDam.txt']
-#x = [xdam,xconf,xpn,xps,xort,xash]
-x = [xps,xpn,xconf,xort,xash,xdam]
+#location of output to be read (fort.qXXXX and fort.tXXXX)
+fortoutputdir = os.path.join(pwd,'_output')
 
-f1 =129
-f2 =129
-frames = range(f1,f2+1)
+#directory to store time series output files
+outdirname = os.path.join(pwd,'_timeseries')
+if not os.path.isdir(outdirname):
+	cmdstr = 'mkdir '+outdirname
+	os.system(cmdstr)
 
-pts = range(len(frames))
+#time series locations (x,y). list directly or optionally read a file
+xlower = 511024.0
+xupper = 608976.0
+ylower = 5361024.0
+yupper = 5425976.0
+x = [] # list of locations
+xp1 = (xlower + 500, ylower + 500)
+x.append(xp1)
+xp2 = (xlower + 1000, ylower + 1000)
+x.append(xp2)
 
-#q0 = np.array([0,0,0,0,0,0,0,0,0])
-Mps = np.loadtxt(infiles[0])
-Mpn = np.loadtxt(infiles[1])
-Mconf = np.loadtxt(infiles[2])
-Mort = np.loadtxt(infiles[3])
-Mash = np.loadtxt(infiles[4])
-Mdam  = np.loadtxt(infiles[5])
+#number of gauges (locations)
+Ng = len(x) 
+Ngs = range(Ng)
 
+#create files to store time series, or read files if they exist
+#for each file/location create array to be put in container list of arrays 
+#files and arrays have columns t,q1,...,q8. One row for each frame number
 
-for pt in pts:
+#Note: this routine assumes that all preexisting time series files have the same number of rows/frames
+Mlist = []
+for ig in Ngs:
+	tsfname = 'Gauge_'+str(ig)+'.txt'
+	fname = os.path.join(outdirname,tsfname)
+	if os.path.isfile(fname):
+		M = np.loadtxt(fname)
+	else:
+		#create array and add first row for t=0.
+		fqname = os.path.join(fortoutputdir,'fort.q0000')
+		ftname = os.path.join(fortoutputdir,'fort.t0000')
+		ftheader = cf.forttheaderread(ftname)
+		t = ftheader['time']
+		print('reading frame '+fqname)
+		solutionlist = cf.fort2list(fqname,ftname)
+		xig = x[ig]
+		qig=cf.pointfromfort(xig,solutionlist)
+		tqig = np.hstack((t,qig))
+		M = np.reshape(tqig,(1,9))
 
-	frame = frames[pt]
+	nrows = len(M)
+	Mlist.append(M)
+	
+
+# frame numbers (fort.qXXX0 - fort.qXXXN) f1 - f2.
+#Note: f1 based on the size of M from above. To restrict range of fort files read, modify f1 
+f1 = nrows #note: begin at number of rows due to 0 index for fort.q0000
+f2 = 3 #choose final frame (could count #fort. files instead)
+frames = range(f1,f2+1) #which frames
+frameN = range(len(frames)) #frame indices
+
+#NOTE: this routine needs to complete to save progress of reading fort files: should probably be improved later
+#loop through frames selected
+for fi in frameN:
+	frame = frames[fi]
 	framex = str(1000 + frame)
 	framename = framex[1:]
-	fqname = os.path.join('_output','fort.q0' + framename)
-	ftname = os.path.join('_output','fort.t0' + framename)
+	fqname = os.path.join(fortoutputdir,'fort.q0' + framename)
+	ftname = os.path.join(fortoutputdir,'fort.t0' + framename)
+	print('reading frame '+fqname)
 	ftheader = cf.forttheaderread(ftname)
 	t = ftheader['time']
-	print('reading frame '+fqname)
 	solutionlist = cf.fort2list(fqname,ftname)
 
-	q=cf.pointfromfort(xdam,solutionlist)
-	qn = np.hstack((t,q))
-	Mdam=np.vstack((Mdam,qn))
+	#loop through number of points (gauges) to find in frame/solution
+	for ig in Ngs:
+		#location of gauge
+		xig = x[ig]
+		#solution at gauge
+		qig=cf.pointfromfort(xig,solutionlist)
+		tqig = np.hstack((t,qig))
+		M = Mlist[ig]
+		M=np.vstack((M,tqig))
+		Mlist[ig] = M
 
-	q=cf.pointfromfort(xconf,solutionlist)
-	qn = np.hstack((t,q))
-	Mconf=np.vstack((Mconf,qn))
 
-	q=cf.pointfromfort(xpn,solutionlist)
-	qn = np.hstack((t,q))
-	Mpn=np.vstack((Mpn,qn))
+#save the arrays to files (THIS WILL OVERWRITE THE PREVIOUS TIME SERIES FILES)	
+for ig in Ngs:
+	M = Mlist[ig]
+	tsfname = 'Gauge_'+str(ig)+'.txt'
+	fname = os.path.join(outdirname,tsfname)
+	np.savetxt(fname,M)
 
-	q=cf.pointfromfort(xps,solutionlist)
-	qn = np.hstack((t,q))
-	Mps=np.vstack((Mps,qn))
 
-	q=cf.pointfromfort(xort,solutionlist)
-	qn = np.hstack((t,q))
-	Mort=np.vstack((Mort,qn))
-
-	q=cf.pointfromfort(xash,solutionlist)
-	qn = np.hstack((t,q))
-	Mash=np.vstack((Mash,qn))
-
-np.savetxt('GaugeAlderDam.txt',Mdam)
-np.savetxt('GaugePuyallupConfluence.txt',Mconf)
-np.savetxt('GaugePuyallupNorth.txt',Mpn)
-np.savetxt('GaugePuyallupSouth.txt',Mps)
-np.savetxt('GaugeOrting.txt',Mort)
-np.savetxt('GaugeAshford.txt',Mash)
-
-"""
-
-"""
 
 
 
--- MtBaker/makestages.py
+++ MtBaker/makestages.py
@@ -1,6 +1,11 @@
 import os
 import numpy as np
 import dclaw.fortconvert as cf
+"""
+this routine creates time series output at chosen locations from fort.q files.
+for time series with higher time resolution than fort output use the gauge feature prior to runtime
+script is intended to be run from top-level simulation directory above _output/.
+"""
 
 #get current directory
 pwd = os.path.abspath('./')
@@ -11,23 +16,29 @@ fortoutputdir = os.path.join(pwd,'_output')
 #directory to store time series output files
 outdirname = os.path.join(pwd,'_timeseries')
 if not os.path.isdir(outdirname):
-	cmdstr = 'makedir '+outdirname
+	cmdstr = 'mkdir '+outdirname
 	os.system(cmdstr)
 
 #time series locations (x,y). list directly or optionally read a file
+xlower = 511024.0
+xupper = 608976.0
+ylower = 5361024.0
+yupper = 5425976.0
 x = [] # list of locations
-xp1 = (5.7385e5,5.1945e6)
+xp1 = (xlower + 500, ylower + 500)
 x.append(xp1)
-xp2 = (5.76345e5,5.1958e6)
+xp2 = (xlower + 1000, ylower + 1000)
 x.append(xp2)
 
 #number of gauges (locations)
 Ng = len(x) 
-Ngs = range(1,Ng+1)
+Ngs = range(Ng)
 
 #create files to store time series, or read files if they exist
 #for each file/location create array to be put in container list of arrays 
 #files and arrays have columns t,q1,...,q8. One row for each frame number
+
+#Note: this routine assumes that all preexisting time series files have the same number of rows/frames
 Mlist = []
 for ig in Ngs:
 	tsfname = 'Gauge_'+str(ig)+'.txt'
@@ -35,39 +46,40 @@ for ig in Ngs:
 	if os.path.isfile(fname):
 		M = np.loadtxt(fname)
 	else:
-		#create file and add first row for t=0.
+		#create array and add first row for t=0.
 		fqname = os.path.join(fortoutputdir,'fort.q0000')
 		ftname = os.path.join(fortoutputdir,'fort.t0000')
 		ftheader = cf.forttheaderread(ftname)
 		t = ftheader['time']
+		print('reading frame '+fqname)
 		solutionlist = cf.fort2list(fqname,ftname)
 		xig = x[ig]
 		qig=cf.pointfromfort(xig,solutionlist)
 		tqig = np.hstack((t,qig))
-		M = tqig
+		M = np.reshape(tqig,(1,9))
 
-	Mlist.append(M)
 	nrows = len(M)
+	Mlist.append(M)
+	
 
 # frame numbers (fort.qXXX0 - fort.qXXXN) f1 - f2.
 #Note: f1 based on the size of M from above. To restrict range of fort files read, modify f1 
 f1 = nrows #note: begin at number of rows due to 0 index for fort.q0000
-f2 = 360 #choose final frame (could count #fort. files instead)
+f2 = 3 #choose final frame (could count #fort. files instead)
 frames = range(f1,f2+1) #which frames
 frameN = range(len(frames)) #frame indices
 
 #NOTE: this routine needs to complete to save progress of reading fort files: should probably be improved later
 #loop through frames selected
 for fi in frameN:
-
 	frame = frames[fi]
 	framex = str(1000 + frame)
 	framename = framex[1:]
 	fqname = os.path.join(fortoutputdir,'fort.q0' + framename)
 	ftname = os.path.join(fortoutputdir,'fort.t0' + framename)
+	print('reading frame '+fqname)
 	ftheader = cf.forttheaderread(ftname)
 	t = ftheader['time']
-	print('reading frame '+fqname)
 	solutionlist = cf.fort2list(fqname,ftname)
 
 	#loop through number of points (gauges) to find in frame/solution
@@ -79,6 +91,7 @@ for fi in frameN:
 		tqig = np.hstack((t,qig))
 		M = Mlist[ig]
 		M=np.vstack((M,tqig))
+		Mlist[ig] = M
 
 
 #save the arrays to files (THIS WILL OVERWRITE THE PREVIOUS TIME SERIES FILES)	
